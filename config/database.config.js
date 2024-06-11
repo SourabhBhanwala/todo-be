94ce01a69edcdf67e63d685632a119b1523bfc73
sourabhbhanwala <sourabhbhanwala1998@gmail.com>
2024-06-11 17:55:17 +0530

    connection string updated

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/todos'
+    url:'mongodb+srv://test-user:K1b1eL2fyeOtVxJF@cluster0.wqmjtdn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+   // url: 'mongodb://localhost:27017/todos'
 }
